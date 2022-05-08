f454d5225ce9a9b96429e3a386ed2bed0c04455b
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-05-08 19:39:04 +0300

    create sign up POST method

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,7 @@
-from flask import render_template
+from flask import render_template,request,flash
 from . import auth
 
-@auth.route('/login')
+@auth.route('/login',methods = ["GET","POST"])
 def login():
     return render_template("auth/login.html")
 
@@ -9,6 +9,23 @@ def login():
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up',methods = ["GET","POST"])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName  = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+            flash('Email must be greater than three characters!',category='error')
+        elif len(firstName) < 2:
+            flash('Name must be greater than one characters!',category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match!',category='error')
+        elif len(password1) < 7:
+            flash('Password must be atleast seven characters!',category='error')
+        else:
+            #add user to db
+            flash('Account created successfully!',category='success')
     return render_template("auth/sign_up.html")
