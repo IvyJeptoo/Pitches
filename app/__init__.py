ff68830ff884b3bed8923eb389fd8114ae14540b
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-05-08 21:13:12 +0300

    add database uri to init

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '1234'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
     from .main import main as main_blueprint
     from .auth import auth as auth_blueprint
