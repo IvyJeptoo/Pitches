823be7ac519f06979fc11b995e17bd87b9634e32
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-05-11 13:06:16 +0300

    deployment

--- app/__init__.py
+++ app/__init__.py
@@ -7,15 +7,19 @@ from flask_simplemde import SimpleMDE
 import os
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 
+
 # app initialization
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pitch.db'
 
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+
 # flask extensions initialization
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
-mail = Mail()
 login_manager = LoginManager(app)
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -34,10 +38,11 @@ MAIL_PASSWORD=os.environ.get('EMAIL_PASS')
 def create_app(config_name):
     # app configurations
     app.config.from_object(config_options[config_name])
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    
     
     #mail instance
-    mail.init_app(app)
+    
     
     
     #main blueprint
